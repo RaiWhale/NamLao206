bf9aa16926ac0303d295a23bc7e3cc2957a95547
Rai <43000634+RaiWhale@users.noreply.github.com>
2025-10-23 07:31:17 +0700

    up

---
+++ Areas/Admin/Controllers/GroupPermissionsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NamLao206.Models;
+
+namespace NamLao206.Areas.Admin.Controllers
+{
+    public class GroupPermissionsController : Controller
+    {
+        private namlao206_websiteEntities db = new namlao206_websiteEntities();
+
+        // GET: Admin/GroupPermissions
+        public async Task<ActionResult> Index()
+        {
+            var groupPermissions = db.GroupPermissions.Include(g => g.PermissionGroup).Include(g => g.Permission);
+            return View(await groupPermissions.ToListAsync());
+        }
+
+        // GET: Admin/GroupPermissions/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupPermission groupPermission = await db.GroupPermissions.FindAsync(id);
+            if (groupPermission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groupPermission);
+        }
+
+        // GET: Admin/GroupPermissions/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupId = new SelectList(db.PermissionGroups, "Id", "GroupName");
+            ViewBag.PermissionId = new SelectList(db.Permissions, "Id", "PermissionName");
+            return View();
+        }
+
+        // POST: Admin/GroupPermissions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,GroupId,PermissionId,CreatedDate,CreatedBy")] GroupPermission groupPermission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GroupPermissions.Add(groupPermission);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupId = new SelectList(db.PermissionGroups, "Id", "GroupName", groupPermission.GroupId);
+            ViewBag.PermissionId = new SelectList(db.Permissions, "Id", "PermissionName", groupPermission.PermissionId);
+            return View(groupPermission);
+        }
+
+        // GET: Admin/GroupPermissions/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupPermission groupPermission = await db.GroupPermissions.FindAsync(id);
+            if (groupPermission == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupId = new SelectList(db.PermissionGroups, "Id", "GroupName", groupPermission.GroupId);
+            ViewBag.PermissionId = new SelectList(db.Permissions, "Id", "PermissionName", groupPermission.PermissionId);
+            return View(groupPermission);
+        }
+
+        // POST: Admin/GroupPermissions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,GroupId,PermissionId,CreatedDate,CreatedBy")] GroupPermission groupPermission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(groupPermission).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupId = new SelectList(db.PermissionGroups, "Id", "GroupName", groupPermission.GroupId);
+            ViewBag.PermissionId = new SelectList(db.Permissions, "Id", "PermissionName", groupPermission.PermissionId);
+            return View(groupPermission);
+        }
+
+        // GET: Admin/GroupPermissions/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupPermission groupPermission = await db.GroupPermissions.FindAsync(id);
+            if (groupPermission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groupPermission);
+        }
+
+        // POST: Admin/GroupPermissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            GroupPermission groupPermission = await db.GroupPermissions.FindAsync(id);
+            db.GroupPermissions.Remove(groupPermission);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/PermissionGroupsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NamLao206.Models;
+
+namespace NamLao206.Areas.Admin.Controllers
+{
+    public class PermissionGroupsController : Controller
+    {
+        private namlao206_websiteEntities db = new namlao206_websiteEntities();
+
+        // GET: Admin/PermissionGroups
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.PermissionGroups.ToListAsync());
+        }
+
+        // GET: Admin/PermissionGroups/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PermissionGroup permissionGroup = await db.PermissionGroups.FindAsync(id);
+            if (permissionGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permissionGroup);
+        }
+
+        // GET: Admin/PermissionGroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PermissionGroups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,GroupName,Description,IsActive,CreatedDate,CreatedBy")] PermissionGroup permissionGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PermissionGroups.Add(permissionGroup);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(permissionGroup);
+        }
+
+        // GET: Admin/PermissionGroups/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PermissionGroup permissionGroup = await db.PermissionGroups.FindAsync(id);
+            if (permissionGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permissionGroup);
+        }
+
+        // POST: Admin/PermissionGroups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,GroupName,Description,IsActive,CreatedDate,CreatedBy")] PermissionGroup permissionGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permissionGroup).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(permissionGroup);
+        }
+
+        // GET: Admin/PermissionGroups/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PermissionGroup permissionGroup = await db.PermissionGroups.FindAsync(id);
+            if (permissionGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permissionGroup);
+        }
+
+        // POST: Admin/PermissionGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            PermissionGroup permissionGroup = await db.PermissionGroups.FindAsync(id);
+            db.PermissionGroups.Remove(permissionGroup);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
